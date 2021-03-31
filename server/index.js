c46d5a1d649951aa57d47a9a3e86474eb4e711f6
DeividasKaraliunas <deikingas@gmail.com>
2021-03-31 10:16:20 +0300

    feat: connected to DB, secured connection

--- server/index.js
+++ server/index.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv').config({ path: 'server/.env' });
 
 const server = express();
+
+const {
+    SERVER_PORT,
+    DB_USER,
+    DB_PASS,
+    DB_URL,
+    DB_NAME
+} = process.env;
 
 server.use(cors());
 server.use(morgan('tiny'));
 
-server.get('/', (req,res) => {
+server.get('/', (req, res) => {
     res.send('Working...')
 });
 
-server.listen(5000, () => {
-    console.log('Server is running on port http://localhost:5000');
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@${DB_URL}/${DB_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('connected', () => {
+    console.log('DB connected')
+    server.listen(SERVER_PORT, () => {
+        console.log(`Server is running on port http://localhost:${SERVER_PORT}`);
+    });
 })
+
